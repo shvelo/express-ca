39fbdb37163c9e1ea9d866179d15a480f407388b
Nick Shvelidze <captain@pirrate.me>
2015-07-02 13:57:58 +0400

    Simple HTTPS Express server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+var https = require('https'),
+	express = require('express');
+
+var credentials = {
+	ca: fs.readFileSync('ssl/ca.crt'),
+	key: fs.readFileSync('ssl/server.key'),
+	cert: fs.readFileSync('ssl/server.crt'),
+	passphrase: 'admin',
+	requestCert: true,
+	rejectUnauthorized: true
+}
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send("Ello govr'nor!");
+});
+
+var server = https.createServer(credentials, app);
+
+server.listen(443);
