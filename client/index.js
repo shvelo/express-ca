4db34c4e7a67afeb0bc6da36739d8e1a56f78f0d
Nick Shvelidze <captain@pirrate.me>
2015-07-02 14:16:28 +0400

    Serve static files

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 var https = require('https'),
+	express = require('express'),
 	fs = require('fs');
+var port = 4000;
 
 var credentials = {
 	ca: fs.readFileSync('ssl/ca.crt'),
@@ -8,17 +10,28 @@ var credentials = {
 	passphrase: 'admin'
 }
 
-https.get({
-	hostname: 'localhost',
-	path: '/',
-	port: 5000,
-	key: credentials.key,
-	ca: credentials.ca,
-	cert: credentials.cert,
-	passphrase: credentials.passphrase,
-	rejectUnauthorized: false
-}, function(res){
-	res.on('data', function(d) {
-		process.stdout.write(d);
+
+var contactServer = function(){
+	https.get({
+		hostname: 'localhost',
+		path: '/',
+		port: 5000,
+		key: credentials.key,
+		ca: credentials.ca,
+		cert: credentials.cert,
+		passphrase: credentials.passphrase,
+		rejectUnauthorized: false
+	}, function(res){
+		res.on('data', function(d) {
+			process.stdout.write(d);
+		});
 	});
-});
+}
+
+var app = express();
+
+app.use(express.static('public'));
+
+var server = app.listen(port);
+
+console.log("Listening on http://localhost:%d", port)
